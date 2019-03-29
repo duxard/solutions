3e9309438baa156bd2b8d2035cbfd8268763eeb5
Aleksandr <duxard@gmail.com>
2019-03-29 13:11:52 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,152 @@
+const cl = console.log;
+
+let a1 = [1,2,3,4,5,6,7,8,9],
+    a2 = ["css", "html", "js"],
+    a3 = [1,1,1,2,2,3,4,5,5],
+    a4 = [1,-2,-3,4,5],
+    a5 = [1,2,3,4,5,6,7,8,9],
+    a6 = [1,2,3],
+    a7 = ['a', 'b', 'c'],
+    a8 = [1,2,3],
+    a9 = ['a', 'b', 'c'],
+    aa8 = [1,2,3],
+    aa9 = ['a', 'b', 'c'],
+    s1 = "  asta ., astAo edwe we  weasta   we asFAFt a ";
+
+
+
+//1
+let na1 = a1.slice(3); //first three
+let na2 = a1.splice(0,3); //last 3
+let na3 = a1.splice(3);   //last 3 - ...
+
+//2
+let na4 = a5.shift();
+
+//3
+a6.push(...a7);
+Array.prototype.push.apply(a8,a9);
+
+//4
+a2.forEach(el => el.length);
+
+let a2n = a2.map(el => el.length);
+
+let sum = a1.reduce((currVal, nextVal) => {
+    return currVal+=nextVal;
+}, 0);
+
+var max = Math.max.apply(null, a5);
+
+//5
+cl( !!~s1.search(/FAF/) );
+cl( /faf/ig.test(s1) );
+
+//6
+let matches = s1.match(/asta/gi).length;
+
+//7
+function foo1(...args){
+    return Array.prototype.slice.call(arguments);
+}
+
+function foo2(){
+    return Array.prototype.join.call(arguments,',');
+}
+
+//8
+function sumA(a){
+    return function(b){
+        return a+b;
+    }
+}
+
+function sumB(a){
+    let res = a;
+    
+    function f(b){
+        res += b;
+        return f;
+    }
+    
+    f.toString = function(){
+        return res;
+    }
+    
+    return f;
+}
+
+//9
+
+function rand(min, max){
+    return Math.floor(min + Math.random()*(max + 1 - min));
+}
+
+let arr1 = [];
+for(let i=0; i<=3; i++){
+    arr1[i] = [];
+    for(let j=0; j<=3; j++){
+        arr1[i][j] = rand(1,100);
+    }
+}
+
+//10
+let o1 = { x: null };
+Object.defineProperty(o1, 'propX', {
+    set: function(val){
+        this.x = val;
+    },
+    get: function(){
+        return this.x;
+    }
+})
+
+
+let o2 = { 
+    x: null,
+    get propX(){
+        return this.x;
+    },
+    set propX(val){
+        this.x = val;
+    }
+};
+
+//11
+
+function Coffee(){
+    let waterAmount = 0;
+    
+    this.waterAmount = function(amount){
+        if(!arguments.length){
+            return waterAmount;
+        } else {
+            waterAmount = amount;
+        }
+    }
+}
+
+let cup = new Coffee();
+cup.waterAmount(100);
+cl(cup.waterAmount())
+
+//14
+Array.prototype.push = (function(){
+    let original = Array.prototype.push;
+    return function(){
+        //...
+        return original.apply(this, arguments);
+    }
+})();
+
+let arr2 = [];
+arr2.join = function(){
+    //...
+    return Array.prototype.join.apply(this, arguments);
+}
+
+
+
+
+
+
