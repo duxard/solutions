e774a51becca47684e58c6e2e5fed82a909f57ed
Aleksandr <duxard@gmail.com>
2019-05-30 11:49:05 -0400

    add sorting

---
+++ js/sorting.js
@@ -0,0 +1,12 @@
+function bubble(arr){
+    let n = arr.length;
+    for(let i=0; i<n-1; i++){
+        for(let j=0; j<n-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                let tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+}
