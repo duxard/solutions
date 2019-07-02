b348bb3eb0aaf9afd0de3e4b3777be0599e974ef
duxard <duxard@gmail.com>
2019-07-02 22:23:14 +0300

    add mixins

---
+++ js/mixins.js
@@ -0,0 +1,30 @@
+let additionalAPI = {
+    sayName(){
+        console.log(this.name);
+    }
+};
+
+class Dog {
+    constructor(name){
+        this.name = name;
+    }
+
+    bark(){
+        console.log('Woof!');
+    }
+}
+
+class Shepherd extends Dog {
+    constructor(name){
+        super(name);
+    }
+}
+
+Object.assign(Shepherd.prototype, additionalAPI);
+
+
+/* usage */
+let d = new Shepherd('Lessy');
+d.bark()
+d.sayName();
+        
