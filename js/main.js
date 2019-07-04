e134ddea6d54a1a9a13fe8ae4713ccab276a1a8e
Alexander <duxard@gmail.com>
2019-07-05 00:12:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -559,3 +559,15 @@ function fizzbuzz(){
 function isIsogram(str){
     return (new Set(str.split(''))).size === str.length;
 }
+
+const iterable1 = new Object();
+
+iterable1[Symbol.iterator] = function* () {
+  yield 1;
+  yield 2;
+  yield 3;
+};
+
+console.log([...iterable1]);
+// expected output: Array [1, 2, 3]
+
