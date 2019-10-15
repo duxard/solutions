3b3da67b5e0308bfb4c25a6ce26886e21410e10c
Alexander <duxard@gmail.com>
2019-10-15 11:01:05 -0400

    bubble sort refactor to use destructuring for  elements swapping

--- js/sorting.js
+++ js/sorting.js
@@ -3,9 +3,7 @@ function bubble(arr){
     for(let i=0; i<n-1; i++){
         for(let j=0; j<n-1-i; j++){
             if(arr[j]>arr[j+1]){
-                let tmp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = tmp;
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
             }
         }
     }
