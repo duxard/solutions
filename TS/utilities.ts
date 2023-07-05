bdbd9e9efd8fead6ddb755d0faf7d6d8d5a98484
Aleksandr <duxard@gmail.com>
2023-07-06 00:04:53 +0300

    Update utilities.ts

--- TS/utilities.ts
+++ TS/utilities.ts
@@ -9,3 +9,7 @@ export function isNonNull<T>(value: T): value is NonNullable<T> {
 export const nonNullObject = <T extends object>(objectToTest: T): objectToTest is T => {
   return Object.values(objectToTest).every(x => x !== null);
 }
+
+export const exhaustedCheck(param: never): never => {
+  throw new Error('Unprocessed param: ', param);
+}
